d0c572ce05dff27eae569cb2f9a753575a25333c
Sam Dokus <95599199+sdokus@users.noreply.github.com>
2022-03-08 12:12:31 -0500

    Update SingleProduct.js

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,7 +3,7 @@ import { connect, useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchSingleProduct } from '../store/singleProduct';
 import { useCart } from '../hooks/useCart';
-import { fetchNewOrder } from '../store/orders';
+import { fetchNewOrder } from '../store/cart';
 
 const SingleProduct = () => {
   const [quantity, setQuantity] = useState('1');
@@ -34,7 +34,6 @@ const SingleProduct = () => {
         <p>${product.price}</p>
         <p>{product.description}</p>
       </div>
-      {/* <button onClick={() => addProduct(productId, quantity, product.price)}> */}
       <button onClick={() => addToCart(productId, quantity, product.price)}>
         Add To Cart
       </button>
@@ -43,67 +42,3 @@ const SingleProduct = () => {
 };
 export default SingleProduct;
 
-// class SingleProduct extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       quantity: "1",
-//     };
-//   }
-//   componentDidMount() {
-//     this.props.loadProduct(this.props.match.params.productId);
-//   }
-//   addProduct(id, quantity, price) {
-//     this.props.createCart({ id: id, quantity: quantity, salesPrice: price });
-//   }
-//   updateQuantity(evt) {
-//     this.setState({ quantity: evt.target.value });
-//   }
-
-//   render() {
-//     const product = this.props.product;
-//     return (
-//       <div className="card">
-//         <h2>{product.name}</h2>
-//         <img src={product.imageUrl} />
-//         <p>
-//           Quantity:
-//           <input
-//             type="number"
-//             onChange={(e) => this.updateQuantity(e)}
-//             value={this.state.quantity}
-//             min="1"
-//           />
-//         </p>
-//         <p>${product.price}</p>
-//         <button
-//           onClick={(id, price) =>
-//             this.addProduct(
-//               this.props.match.params.productId,
-//               this.state.quantity,
-//               product.price
-//             )
-//           }
-//         >
-//           Add To Cart
-//         </button>
-//         <p>{product.description}</p>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapState = (state) => {
-//   return {
-//     product: state.singleProduct,
-//   };
-// };
-
-// const mapDispatch = (dispatch) => {
-//   return {
-//     loadProduct: (id) => dispatch(fetchSingleProduct(id)),
-//     createCart: (body) => dispatch(addOrderItem(body)),
-//   };
-// };
-
-// export default connect(mapState, mapDispatch)(SingleProduct);
