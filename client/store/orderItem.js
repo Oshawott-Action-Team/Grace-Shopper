33731579face06f36998323018b75d20d83f0798
Sam Dokus <95599199+sdokus@users.noreply.github.com>
2022-03-08 12:20:47 -0500

    Update orderItem.js

--- client/store/orderItem.js
+++ client/store/orderItem.js
@@ -1,18 +1,10 @@
 import Axios from 'axios';
 
 const ADD_PRODUCTS = 'ADD_PRODUCTS';
-const UPDATE_QUANTITY = 'UPDATE_QUANTITY';
 
 export const addProducts = (order) => {
   return {
     type: ADD_PRODUCTS,
-    order,
-  };
-};
-
-export const _updateQuantity = (order) => {
-  return {
-    type: UPDATE_QUANTITY,
     order,
   };
 };
@@ -36,29 +28,10 @@ export const addOrderItem = (product) => {
   };
 };
 
-export const updateQuantity = (product) => {
-  return async (dispatch) => {
-    try {
-      const token = window.localStorage.getItem('token');
-      if (token) {
-        const { data } = await Axios.put('/api/orders/product', product, {
-          headers: {
-            authorization: token,
-          },
-        });
-        dispatch(_updateQuantity(data));
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-};
 
 export default function orderItemReducer(state = {}, action) {
   switch (action.type) {
     case ADD_PRODUCTS:
-      return action.order;
-    case UPDATE_QUANTITY:
       return action.order;
     default:
       return state;
